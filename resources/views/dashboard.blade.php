4a519d84cc8fc90ddc5799b11de5cb8f0080dd0c
Irsal Hamdi <irsalhamdi@gmail.com>
2022-08-16 18:20:30 +0700

    Dashboard User

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,4 +1,4 @@
-<x-app-layout>
+{{-- <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
            Hi..{{ Auth::user()->name }}
@@ -8,4 +8,122 @@
     <div class="py-12">
         This is just home page 
     </div>
-</x-app-layout>
+</x-app-layout> --}}
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
+    <meta name="description" content=""/>
+    <meta name="author" content=""/>
+
+    <title>@yield('title')</title>
+
+    @stack('prepend-style')
+    <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet" />
+    <link href="/dashboard-user/style/main.css" rel="stylesheet" />
+    @stack('addon-style')
+  </head>
+
+  <body>
+    <div class="page-dashboard">
+      <div class="d-flex" id="wrapper" data-aos="fade-right">
+        <!-- Sidebar -->
+        <div class="border-right" id="sidebar-wrapper">
+          <div class="sidebar-heading text-center">
+            <img src="/dashboard-user/images/dashboard-store-logo.svg" alt="" class="my-4" />
+          </div>
+          <div class="list-group list-group-flush">
+            <a href="" class="list-group-item list-group-item-action {{ (request()->is('dashboard')) ? 'active' : '' }}">
+              Dashboard
+            </a>
+            <a href="" class="list-group-item list-group-item-action">
+              Profile
+            </a>
+            <a href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();" class="list-group-item list-group-item-action">
+              Sign Out
+            </a>
+            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                @csrf
+            </form>
+          </div>
+        </div>
+
+        <!-- Page Content -->
+        <div id="page-content-wrapper">
+          <nav class="navbar navbar-expand-lg navbar-light navbar-store fixed-top" data-aos="fade-down">
+            <div class="container-fluid">
+              <button class="btn btn-success d-md-none mr-auto mr-2" id="menu-toggle" >
+                &laquo; Menu
+              </button>
+              <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent">
+                <span class="navbar-toggler-icon"></span>
+              </button>
+              <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <!-- Desktop Menu -->
+                <ul class="navbar-nav d-none d-lg-flex ml-auto">
+                  <li class="nav-item dropdown">
+                    <a href="#" class="nav-link" id="navbarDropdown" role="button" data-toggle="dropdown" >
+                        <img src="/dashboard-user/images/icon-user.png" alt="" class="rounded-circle mr-2 profile-picture" />
+                        Hi, {{ Auth::user()->name }}
+                    </a>
+                    <div class="dropdown-menu">
+                        <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+                            Logout
+                        </a>
+                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                            @csrf
+                        </form>
+                    </div>
+                  </li>
+                  <li class="nav-item">
+                    <a href="" class="nav-link d-inline-block mt-2">
+                      @php
+                          $users = \App\Models\User::where('id', Auth::user()->id)->get();
+                      @endphp
+                      @if($users)
+                          <img src="/dashboard-user/images/icon-cart-filled.svg" alt="" />
+                          <div class="card-badge">1</div>
+                      @else
+                          <img src="/dashboard-user/images/icon-cart-empty.svg" alt="" />
+                      @endif
+                  </a>
+                </li>
+                  </li>
+                </ul>
+
+                <ul class="navbar-nav d-block d-lg-none">
+                    <li class="nav-item">
+                        <a href="{{ route('dashboard') }}" class="nav-link">
+                            Hi, {{ Auth::user()->name }}
+                        </a>
+                    </li>
+                </ul>    
+              </div>
+            </div>
+          </nav>
+
+          {{-- Content --}}
+          @yield('content')
+
+        </div>
+      </div>
+    </div>
+
+    <!-- Bootstrap core JavaScript -->
+    @stack('prepend-script')
+    <script src="/dashboard-user/vendor/jquery/jquery.slim.min.js"></script>
+    <script src="/dashboard-user/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+    <script>
+      AOS.init();
+    </script>
+    <script>
+      $("#menu-toggle").click(function (e) {
+        e.preventDefault();
+        $("#wrapper").toggleClass("toggled");
+      });
+    </script>
+    @stack('addon-script')
+  </body>
+</html>
