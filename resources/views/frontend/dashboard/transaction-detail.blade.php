a9accbafb9e4e0a3cecf74d4d6656b6d19bfe980
Irsal Hamdi <irsalhamdi@gmail.com>
2022-08-18 14:37:25 +0700

    Fixing UI Payment

--- resources/views/frontend/dashboard/transaction-detail.blade.php
+++ resources/views/frontend/dashboard/transaction-detail.blade.php
@@ -34,39 +34,51 @@
                       </ul>
                       <div class="tab-content" id="pills-tabContent">
                         <div class="tab-pane fade show active"  id="pills-destination"  role="tabpanel" aria-labelledby="pills-destination-tab" >
-                              <a href="" class="card card-list d-block">
-                                <div class="card-body">
-                                  <div class="row">
-                                    <div class="col-md-6">
-                                      <img src="/{{ $transaction->destination->image }}" width="350" height="150" class="mb-3"/> 
+                          <a href="" class="card card-list d-block">
+                            <div class="card-body">
+                              <div class="row">
+                                <div class="col-md-6">
+                                  <p class="dashboard-subtitle">
+                                    Destination
+                                  </p>
+                                  <img src="/{{ $transaction->destination->image }}" width="350" height="150" class="mb-3"/> 
                                         @php
                                             $galleries = App\Models\Gallery::where('destination_id', $transaction->id)->get();
                                         @endphp
                                         @foreach ($galleries as $gallery)
                                             <img src="{{ asset($gallery->image) }}" width="350" height="150" class="mb-3"/>
                                         @endforeach
-                                    </div>
-                                    <div class="col-md-6">
-                                        <p class="dashboard-subtitle">
-                                             {{ $transaction->destination->destinationtype->name }} / {{ $transaction->destination->village->name }} / {{ $transaction->destination->name }}
-                                        </p>
-                                        <p style="text-align: justify">
-                                            {{ $transaction->destination->description   }}
-                                        </p>
-                                        <button class="btn btn-sm btn-success">
-                                            <i class="fas fa-delete"></i>
-                                            {{ $transaction->destination->guide }}
-                                        </button>
-                                        <button class="btn btn-sm btn-danger ml-3">
-                                            {{ $transaction->destination->price }}
-                                        </button>
-                                        {{-- <a class="btn btn-sm btn-primary ml-3" href="{{ $transaction->destination->location }}">
-                                            Location
-                                        </a> --}}
-                                    </div>
-                                  </div>
                                 </div>
-                              </a>
+                                <div class="col-md-6">
+                                  <p class="dashboard-subtitle">
+                                   {{ $transaction->destination->destinationtype->name }} / {{ $transaction->destination->village->name }} / {{ $transaction->destination->name }}
+                                  </p>
+                                  <p style="text-align: justify">
+                                    {{ $transaction->destination->description   }}
+                                  </p>
+                                  <button class="btn btn-sm btn-success">
+                                    <i class="fas fa-delete"></i>
+                                    {{ $transaction->destination->guide }}
+                                  </button>
+                                  <button class="btn btn-sm btn-danger ml-3">
+                                    {{ $transaction->destination->price }}
+                                  </button>
+                                  {{-- <a class="btn btn-sm btn-primary ml-3" href="{{ $transaction->destination->location }}">
+                                    Location
+                                  </a> --}}
+                                </div>
+                              </div>
+                              <div class="row">
+                                <div class="col-md-6">
+                                  <p class="dashboard-subtitle">
+                                    Transportation
+                                  </p>
+                                  <img src="/{{ $transaction->transportation->image }}" width="350" height="150" class="mb-3"/> 
+                                </div>
+                                <div class="col-md-6"></div>
+                              </div>
+                            </div>
+                          </a>
                         </div>
                         <div class="tab-pane fade" id="pills-hotel" role="tabpanel" aria-labelledby="pills-hotel-tab">
                             <a href="" class="card card-list d-block">
@@ -86,9 +98,31 @@
                                 <div class="card-body">
                                   <div class="row">
                                     <div class="col-md-6">
-                                      <img src="/{{ $transaction->transportation->image }}" width="350" height="150" class="mb-3"/> 
+                                      Hello, {{ Auth::user()->name }}
+                                      <h2>
+                                        Rp. {{ $transaction->total_price }}
+                                      </h2>
+                                      <p class="dashboard-subtitle" style="text-align: justify">
+                                        Please, send your payment proof ! <br>  
+                                        # {{ $transaction->code }}
+                                      </p>
                                     </div>
                                     <div class="col-md-6">
+                                      <form action="{{ route('payment-proof',$transaction->id) }}" method="POST" enctype="multipart/form-data">
+                                        @csrf
+                                        <div class="form-group mt-3">
+                                          <h5>Image</h5>
+                                          <div class="controls">
+                                              <input type="file" name="image" class="form-control" required="" id="image">
+                                          </div>
+                                        </div>
+                                        <img id="showImage" src="{{ url('upload/default.jpg') }}" style="width: 100px; height: 100px;" class="mb-3">
+                                        <div class="col text-right">
+                                          <button type="submit" class="btn btn-success px-5">
+                                              Submit
+                                          </button>
+                                        </div>
+                                      </form>
                                     </div>
                                   </div>
                                 </div>
@@ -102,3 +136,16 @@
         </div>
     </div>
 @endsection
+@push('addon-script')
+  <script type="text/javascript">
+    $(document).ready(function(){
+      $('#image').change(function(e){
+        var reader = new FileReader();
+        reader.onload = function(e){
+        $('#showImage').attr('src',e.target.result);	
+        }
+        reader.readAsDataURL(e.target.files['0']);
+      });
+    });
+  </script>
+@endpush
